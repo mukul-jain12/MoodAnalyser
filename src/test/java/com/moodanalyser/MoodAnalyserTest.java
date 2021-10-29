b7ad0701db66e3f2435f651756b9b25d86199c43
Mukul Jain <mukuljain422@gmail.com>
2021-10-29 22:14:30 +0530

    sad test case

--- src/test/java/com/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/moodanalyser/MoodAnalyserTest.java
@@ -5,9 +5,9 @@ import org.junit.Test;
 
 public class MoodAnalyserTest {
 	@Test
-    public void givenMessage_IsProper_ShouldReturnSad() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String actualResult = moodAnalyser.analyseMood("I am Sad in Mood");
-        Assert.assertEquals("SAD", actualResult);
-    }
+	public void givenMessage_IsProper_ShouldReturnSad() {
+		MoodAnalyser moodAnalyser = new MoodAnalyser();
+		String actualResult = moodAnalyser.analyseMood("I am Sad in Mood");
+		Assert.assertEquals("SAD", actualResult);
+	}
 }
